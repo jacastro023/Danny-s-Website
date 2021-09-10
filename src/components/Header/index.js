41b4865fe404373eab8ec953a8be5fc2d10cc667
Jose Castro <jacastro023@gmail.com>
2021-09-10 12:52:04 -0500

    added logo and css to header and footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import logo from "./logo.png";
 
 
 function Header() {
   return (
     <Navbar variant="dark" bg="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="/Home">Danny Gamer</Navbar.Brand>
+        <Navbar.Brand href="/Home">
+        <img src={logo} alt="logo" /> Danny <br></br>Gamer
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-dark-example" />
         <Navbar.Collapse id="navbar-dark-example">
           <Nav class="ml-auto">
